4fdb3d2204b4fab6e562f9e4fc7673436d1d2af1
jauhar97 <jauharm97@gmail.com>
2022-08-14 23:43:46 +0500

    third com

--- Route/stackNavigation.js
+++ Route/stackNavigation.js
@@ -6,6 +6,11 @@ import Signup from '../App/Screen/Signup';
 import Login from '../App/Screen/Login';
 import Language from '../App/Screen/Language';
 import MyTabs from './tabNavigation'
+import Inbox from '../Screens/Inbox';
+import Offer from '../Screens/Offer';
+import Profile from '../Screens/Profile';
+import Ride from '../Screens/Ride';
+import Seacrh from '../Screens/Search';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,6 +23,11 @@ const StackScreen = () => {
   }} >
                 <Stack.Screen name='tabs' component={MyTabs}/>
                 <Stack.Screen name='Signup' component={Signup}/>
+                <Stack.Screen name='Inbox' component={Inbox}/>
+                <Stack.Screen name='Offer' component={Offer}/>
+                <Stack.Screen name='Profile' component={Profile}/>
+                <Stack.Screen name='Ride' component={Ride}/>
+                <Stack.Screen name='Seacrh' component={Seacrh}/>
                 <Stack.Screen name='Login' component={Login}/>
                 <Stack.Screen name='CurrentLocation' component={CurrentLocation}/>
                 <Stack.Screen name='Language' component={Language}/>
--- Screens/Profile.js
+++ Screens/Profile.js
@@ -1,12 +1,38 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Container, Header, Item, Input, Button, Tab, Tabs } from 'native-base';
+import Color from '../App/config/Color';
 
-export default function Profile() {
+
+ function Profile() {
   return (
     <View>
-      <Text>Profile</Text>
+     <Tabs tabContainerStyle={{ elevation: 0, width:'90%', alignSelf:'center',marginTop:5,alignContent:'center' }} 
+                  tabBarUnderlineStyle={{ backgroundColor: '#0fd3bb', height: 0,width:50,marginLeft:30,}}>
+        <Tab
+            tabStyle={{ backgroundColor:Color.white, }}
+            activeTabStyle={{ backgroundColor: Color.white, }}
+            textStyle={{ color: '#030303', fontWeight: 'bold' }}
+            activeTextStyle={{ color: '#030303', fontWeight: 'bold', fontSize: 20 }}
+            heading="About">
+                    
+                        
+               <Text>Furqan</Text>
+        </Tab>
+        <Tab
+                    tabStyle={{ backgroundColor: Color.white , }}
+                    activeTabStyle={{ backgroundColor: Color.white , }}
+                    textStyle={{ color: '#030303', fontWeight: 'bold',alignSelf:'center' }}
+                    activeTextStyle={{ color: '#030303', fontWeight: 'bold', fontSize: 20,alignSelf:'center' }}
+                    heading="Gigs">
+                   <Text>hassan</Text>
+        </Tab>            
+    </Tabs>
     </View>
   )
 }
+export default Profile
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    
+})
